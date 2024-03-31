b36ccbd41daf57d8512c023684eced1a00a69f30
automacon-gromoff <a.gromov@automacon.ru>
2024-03-31 13:39:22 +0300

    Task 3.4

---
+++ lesson03/Task3_4/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const (
+	firstConst  = 1
+	secondConst = 12
+	thirdConst  = 3
+	fourthConst = 68
+	fifthConst  = 121
+)
+
+func main() {
+	fmt.Println(firstConst, secondConst, thirdConst, fourthConst, fifthConst)
+}
