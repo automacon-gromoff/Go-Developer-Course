fcafe2ee57907d8e70043630a373648a6647251c
automacon-gromoff <a.gromov@automacon.ru>
2024-04-25 15:20:29 +0300

    Remake

--- lesson10/task10_4/go.mod
+++
@@ -1,8 +0,0 @@
-module lesson
-
-go 1.22.0
-
-require (
-	github.com/automacon-gromoff/v2_module v1.0.0 // indirect
-	github.com/automacon-gromoff/v2_module/v2 v2.0.0 // indirect
-)
--- lesson10/task10_4/main.go
+++ lesson10/task10_4/main.go
@@ -1,12 +1,4 @@
 package main
 
-import (
-	"fmt"
-	v1 "github.com/automacon-gromoff/v2_module"
-	v2 "github.com/automacon-gromoff/v2_module/v2"
-)
-
 func main() {
-	fmt.Println(v1.Hello())
-	fmt.Println(v2.Hello())
 }
